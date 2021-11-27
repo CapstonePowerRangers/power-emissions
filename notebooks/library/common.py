05be75abdf8b07735d6322e7eb6d168b86c3e07e
hokoonleong <klho@umich.edu>
2021-11-27 17:53:46 +0800

    Created Core class in library common to hold common parameters tbe used across the project Modified create_model.ipynb to use Core class

--- notebooks/library/common.py
+++ notebooks/library/common.py
@@ -12,3 +12,59 @@ class Database:
         self.engine = sqlalchemy.create_engine(self.config)
         self.connection = self.engine.connect()
         self.metadata = sqlalchemy.MetaData()
+
+# Must always have these two features
+class Core:
+    def __init__(self):
+        _essential = ['iso_code', 'year']
+
+        # These are the complete list of features from the dataset
+        _base = [
+            'co2', 'consumption_co2', 'trade_co2', 'coal_co2', 'cement_co2',
+            'flaring_co2', 'gas_co2', 'oil_co2', 'other_industry_co2',
+            'methane', 'nitrous_oxide', 'population', 'gdp', 'primary_energy_consumption']
+
+        _derived = [
+            'co2_growth_prct', 'co2_growth_abs', 'co2_per_capita', 'consumption_co2_per_capita',
+            'share_global_co2', 'cumulative_co2', 'share_global_cumulative_co2', 'co2_per_gdp',
+            'consumption_co2_per_gdp', 'co2_per_unit_energy', 'cement_co2_per_capita',
+            'coal_co2_per_capita', 'flaring_co2_per_capita', 'gas_co2_per_capita',
+            'oil_co2_per_capita', 'other_co2_per_capita', 'trade_co2_share',
+            'share_global_cement_co2', 'share_global_coal_co2', 'share_global_flaring_co2',
+            'share_global_gas_co2', 'share_global_oil_co2', 'share_global_other_co2',
+            'cumulative_cement_co2', 'cumulative_coal_co2', 'cumulative_flaring_co2',
+            'cumulative_gas_co2', 'cumulative_oil_co2', 'cumulative_other_co2',
+            'share_global_cumulative_cement_co2', 'share_global_cumulative_coal_co2',
+            'share_global_cumulative_flaring_co2', 'share_global_cumulative_gas_co2',
+            'share_global_cumulative_oil_co2', 'share_global_cumulative_other_co2', 'total_ghg',
+            'ghg_per_capita', 'methane_per_capita', 'nitrous_oxide_per_capita',
+            'energy_per_capita', 'energy_per_gdp']
+
+        self.all_features = []
+        self.derived_features = []
+        self.base_features = []
+
+        self.base_features.extend(_essential)
+        self.base_features.extend(_base)
+
+        self.derived_features.extend(_essential)
+        self.derived_features.extend(_derived)
+
+        self.all_features.extend(_essential)
+        self.all_features.extend(_base)
+        self.all_features.extend(_derived)
+
+        self.world = "all"
+        self.regions = {'global': [self.world],
+                        'usa': ['USA'],
+                        'china': ['CHN']
+                        }
+        self.list_of_regions = [*self.regions]
+        self.excluded_features = ['OWID_WRL']
+        #
+        # self.regression_features = self.base_features
+        _selected =[]
+        _selected.extend(_essential)
+        _selected.extend(['consumption_co2'])
+        self.regression_features = self.all_features
+        self.clustering_features = self.base_features
